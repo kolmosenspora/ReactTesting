04aad02f1e5f6c9117a3c05ddf27a3060357c191
kolmosenspora <mikokallio@gmail.com>
2019-07-12 00:40:04 +0300

    initial commit

---
+++ precache-manifest.4d2195815faa96bb03b5e96ed9820b28.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065d8de7ffda76ee6ceb6f56e68fd3ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cae4e70726bc4f4f203",
+    "url": "/static/css/2.5020d6f3.chunk.css"
+  },
+  {
+    "revision": "cb3ce86f82b3b4b631ad",
+    "url": "/static/css/main.199fccc4.chunk.css"
+  },
+  {
+    "revision": "6cae4e70726bc4f4f203",
+    "url": "/static/js/2.01b8338b.chunk.js"
+  },
+  {
+    "revision": "cb3ce86f82b3b4b631ad",
+    "url": "/static/js/main.603f7a83.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "15678ffe04860272db359a247ec7fc08",
+    "url": "/static/media/CSS.3.15678ffe.svg"
+  },
+  {
+    "revision": "1edd5890e466153390aa3707ce848117",
+    "url": "/static/media/HTML5-logo.1edd5890.svg"
+  },
+  {
+    "revision": "6644d69441b0824897abd761026e01e0",
+    "url": "/static/media/Javan_logo.6644d694.svg"
+  },
+  {
+    "revision": "237a7f4405531d3a691ce420bebd9fa6",
+    "url": "/static/media/Javascript_badge.237a7f44.svg"
+  },
+  {
+    "revision": "5e9d46e546278ce2bcc03cfb590a2915",
+    "url": "/static/media/Spring_Framework_Logo_2018.5e9d46e5.svg"
+  },
+  {
+    "revision": "0a5dc749c4f128fa2c303e42842731e5",
+    "url": "/static/media/icon-spring-boot.0a5dc749.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6810f34447ebda0c528026de3dfca31c",
+    "url": "/static/media/postgres.6810f344.svg"
+  }
+]);
